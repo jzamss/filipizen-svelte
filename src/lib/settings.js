c57b249f6e2476bdd1f26138ae527ce48f68a57b
Jessie S. Zamora <jzamss@gmail.com>
2022-07-09 16:11:18 +0800

    setting

--- src/lib/settings.js
+++ src/lib/settings.js
@@ -1,7 +1,10 @@
 const CLOUD_FILIPIZEN_SERVER_IP =
-	import.meta.env.VITE_CLOUD_FILIPIZEN_SERVER_IP || '172.28.222.202:5000';
+	import.meta.env.VITE_CLOUD_FILIPIZEN_SERVER_IP ||
+	process.env.CLOUD_FILIPIZEN_SERVER_IP ||
+	'172.28.222.202:5000';
 
-const CLOUD_GDX_NODE_IP = import.meta.env.VITE_CLOUD_GDX_NODE_IP || '172.28.222.202:3000';
+const CLOUD_GDX_NODE_IP =
+	import.meta.env.VITE_CLOUD_GDX_NODE_IP || process.env.CLOUD_GDX_NODE_IP || '172.28.222.202:3000';
 
 export const cloudFilipizenServerUrl = `http://${CLOUD_FILIPIZEN_SERVER_IP}`;
 
